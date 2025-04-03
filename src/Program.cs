147b55d76e6932cf7810f3b75f44f8f937c9b868
Robert Gasior <robertjohngasior@gmail.com>
2025-04-03 11:17:02 +0200

    made some minor changes

--- src/Program.cs
+++ src/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace Stos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StosWTablicy<string> s = new StosWTablicy<string>(2);
+            s.Push("km");
+            s.Push("aa");
+            s.Push("xx");
+            foreach (var x in s.ToArray())
+                Console.WriteLine(x);
+
+            Console.WriteLine();
+        }
+        static void swap(ref int x, ref int y) {
+            var temp = x;
+            x= y;
+            y = temp;
+        }
+    }
+}
