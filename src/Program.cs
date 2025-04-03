d6cf9a62ab7eacc4e7e5a51322dc99750f63003b
Robert Gasior <robertjohngasior@gmail.com>
2025-04-03 11:00:01 +0200

    making initial commit

--- src/Program.cs
+++ src/Program.cs
@@ -1,19 +1,2 @@
-﻿using System;
-
-namespace Stos
-{
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            StosWTablicy<string> s = new StosWTablicy<string>(2);
-            s.Push("km");
-            s.Push("aa");
-            s.Push("xx");
-            foreach (var x in s.ToArray())
-                Console.WriteLine(x);
-
-            Console.WriteLine();
-        }
-    }
-}
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
